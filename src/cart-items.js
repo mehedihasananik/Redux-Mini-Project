d37f3107dd7669fb37d4fcdc9444b6d0658061b7
marshallanique <marshallanique@gmail.com>
2023-08-16 15:20:20 +0600

    first commit

---
+++ src/cart-items.js
@@ -0,0 +1,27 @@
+export const items = [
+  {
+    id: 1,
+    title: "Samsung Galaxy S7",
+    price: 599.99,
+    img:
+      "https://www.singtel.com/content/dam/singtel/eshop/Mobile/Handset/Apple/iPhone14/iphone14-pro-purple-645b6d-01%20(v2).jpg",
+    amount: 1
+  },
+  {
+    id: 2,
+    title: "google pixel ",
+    price: 499.99,
+    img:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR7C8t1lE0Kao_zpb-d4hphiA2T-njMJ4uoqg&usqp=CAU",
+    amount: 1
+  },
+  {
+    id: 3,
+    title: "Xiaomi Redmi Note 2",
+    price: 699.99,
+    img:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTgvX3DlYmf0eDGbh7VEHP953wY_Q6GZQpqmwo_aW7sA84wzM__2BY5IwY2ME25sKjrmXE&usqp=CAU",
+    amount: 1
+  }
+];
+
